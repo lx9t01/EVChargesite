b03efb5c79407cfad6de859ff47f587ef3b62058
lx9t01 <alex.xuhaixiang@gmail.com>
2016-03-03 12:28:49 -0800

    2016.03.03 current project, need improve on button, url for carID and userID

---
+++ EVCharge/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
+  url(r'^ev/(?P<car_id>[0-9 A-Z a-z]+)$', views.ev, name='ev'),
+  url(r'^ev/(?P<car_id>[0-9 A-Z a-z]+)/(?P<user_id>[0-9]+)/charging/(?P<duration>[0-9 .]+)/(?P<distance>[0-9 .]+)$', views.charging, name='charging'),
+]
---
+++ EVCharge/views.py
@@ -0,0 +1,178 @@
+from django.shortcuts import render
+from pymongo import MongoClient
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+import datetime
+from random import randint
+
+def index(request):
+    template = loader.get_template('EVCharge/index.html')
+    return HttpResponse(template.render(request))
+
+def ev(request, car_id):
+    client = MongoClient('mongodb://ev_user:ee135@ds064188.mlab.com:64188/evdb')
+    db = client['evdb']
+    carCollection = db['car']
+
+    result = carCollection.find_one({"carID": car_id})
+    if result!=None:
+        print(result)
+    template = loader.get_template('EVCharge/EVs.html')
+    context = {
+        'car':result,
+    }
+    return HttpResponse(template.render(context, request))
+
+def charging(request, car_id, user_id, duration, distance):
+    client = MongoClient('mongodb://ev_user:ee135@ds064188.mlab.com:64188/evdb')
+    db = client['evdb']
+    carCollection = db['car']
+    
+    template = loader.get_template('EVCharge/charging.html')
+    return HttpResponse(template.render(context, request))
+    
+    
+# sample data format:
+'''
+carCollection = db['car']
+car1 = {'carID': 'A000001',
+        'make': 'Tesla',
+        'model': 'Model X',
+        'milePerKWh': 3.00,
+        'maxRate': 1.5,
+        'imageLink': '/static/Tesla-Model-X.jpg',
+}
+
+historyCollection = db['history']
+history1 = {'carID': 'A000001',
+        'userID': '2017496',
+        'chargeHistory': [{'si': datetime.datetime(2016, 3, 3, 6, 45, 11, 299893)
+                           'ti': datetime.datetime(2016, 3, 3, 11, 45, 11, 299893)
+                           'ei': 10
+                           'ri': 0.667
+                          },
+                          {'si': datetime.datetime(2016, 3, 2, 8, 45, 11, 299893)
+                           'ti': datetime.datetime(2016, 3, 2, 16, 45, 11, 299893)
+                           'ei': 10
+                           'ri': 0.42
+                          },
+                         ]     
+}
+
+'''
+def populate():
+    client = MongoClient('mongodb://ev_user:ee135@ds064188.mlab.com:64188/evdb')
+    db = client['evdb']
+    carCollection = db['car']
+    car1 = {'carID': 'A000002',
+        'make': 'Tesla',
+        'model': 'Model S',
+        'milePerKWh': 3.00,
+        'maxRate': 1.5,
+        'imageLink': '/static/Tesla-Model-S1.jpg',
+        }
+    uoload_id = carCollection.insert_one(car1).inserted_id
+
+
+def populate_history(carID='A000002', userID='2017496'):
+    chargeHistory = []
+    client = MongoClient('mongodb://ev_user:ee135@ds064188.mlab.com:64188/evdb')
+    db = client['evdb']
+    historyCollection = db['history']
+    # generate data for Thursdays:
+    ratio = 1.1
+    st = datetime.datetime(2015,1,1)
+    dt = datetime.timedelta(days=7)
+    for i in range(50):
+        dh = randint(-2,2)
+        dm = randint(-35, 35)
+        ds = randint(-59, 59)
+        delta = datetime.timedelta(hours=dh) + datetime.timedelta(minutes=dm) + datetime.timedelta(seconds=ds)
+        si = st + i*dt + delta # start time
+        duration = randint(6,9) # expected end time
+        ti = si + datetime.timedelta(hours=ratio*duration+0.3*randint(-1,1)) # real left time
+        ei = 10 * duration + randint (-20, 20)# expected travel distance
+        ri = 1.5
+        ch1 = {'si': si, 'ti': ti, 'duration_exp': duration,
+               'ei': ei, 'ri': ri,
+        }
+        chargeHistory.append(ch1)
+    # Fridays
+    st = datetime.datetime(2015,1,2)
+    dt = datetime.timedelta(days=7)
+    ratio = 0.9
+    for i in range(50):
+        dh = randint(-2,2)
+        dm = randint(-35, 35)
+        ds = randint(-59, 59)
+        delta = datetime.timedelta(hours=dh) + datetime.timedelta(minutes=dm) + datetime.timedelta(seconds=ds)
+        si = st + i*dt + delta # start time
+        duration = randint(4,12) # expected end time
+        ti = si + datetime.timedelta(hours=ratio*duration+0.3*randint(-1,0)) # real left time
+        ei = 10 * duration + randint (-20, 20)# expected travel distance
+        ri = 1.5
+        ch1 = {'si': si, 'ti': ti, 'duration_exp': duration,
+               'ei': ei, 'ri': ri,
+        }
+        chargeHistory.append(ch1)
+    # Mondays
+    st = datetime.datetime(2015,1,5)
+    dt = datetime.timedelta(days=7)
+    ratio = 1.2
+    for i in range(50):
+        dh = randint(0,2)
+        dm = randint(-35, 35)
+        ds = randint(-59, 59)
+        delta = datetime.timedelta(hours=dh) + datetime.timedelta(minutes=dm) + datetime.timedelta(seconds=ds)
+        si = st + i*dt + delta # start time
+        duration = randint(7,10) # expected end time
+        ti = si + datetime.timedelta(hours=ratio*duration+0.3*randint(-1,0)) # real left time
+        ei = 10 * duration + randint (-20, 20)# expected travel distance
+        ri = 1.5
+        ch1 = {'si': si, 'ti': ti, 'duration_exp': duration,
+               'ei': ei, 'ri': ri,
+        }
+        chargeHistory.append(ch1)
+    # Tuesdays
+    st = datetime.datetime(2015,1,6)
+    dt = datetime.timedelta(days=7)
+    ratio = 1.0
+    for i in range(50):
+        dh = randint(-1,1)
+        dm = randint(-35, 35)
+        ds = randint(-59, 59)
+        delta = datetime.timedelta(hours=dh) + datetime.timedelta(minutes=dm) + datetime.timedelta(seconds=ds)
+        si = st + i*dt + delta # start time
+        duration = randint(6,8) # expected end time
+        ti = si + datetime.timedelta(hours=ratio*duration+0.3*randint(-1,0)) # real left time
+        ei = 10 * duration + randint (-20, 20)# expected travel distance
+        ri = 1.5
+        ch1 = {'si': si, 'ti': ti, 'duration_exp': duration,
+               'ei': ei, 'ri': ri,
+        }
+        chargeHistory.append(ch1)
+    # Wednesdays
+    st = datetime.datetime(2015,1,7)
+    dt = datetime.timedelta(days=7)
+    ratio = 1.0
+    for i in range(50):
+        dh = randint(0,2)
+        dm = randint(-35, 35)
+        ds = randint(-59, 59)
+        delta = datetime.timedelta(hours=dh) + datetime.timedelta(minutes=dm) + datetime.timedelta(seconds=ds)
+        si = st + i*dt + delta # start time
+        duration = randint(6,8) # expected end time
+        ti = si + datetime.timedelta(hours=ratio*duration+0.3*randint(-1,0)) # real left time
+        ei = 10 * duration + randint (-20, 20)# expected travel distance
+        ri = 1.5
+        ch1 = {'si': si, 'ti': ti, 'duration_exp': duration,
+               'ei': ei, 'ri': ri,
+        }
+        chargeHistory.append(ch1)
+    history1 = {'carID': carID, 'userID': userID, 
+                'chargeHistory':chargeHistory,
+    }
+    upload_id = historyCollection.insert_one(history1).inserted_id
+    
+    
